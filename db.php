cd3734f3e365e328da0e18f52d0d90cb3872840a
ymlair <hpmarkup@gmail.com>
2018-11-10 19:56:01 +0800

    add

--- db.php
+++ db.php
@@ -1,9 +1,10 @@
 <?php
 return [
     'database_type' => 'mysql',
-    'database_name' => 'demo',
+    'database_name' => 'flashcode',
     'server' => '127.0.0.1',
-    'username' => 'one-root',
+    'username' => 'root',
     'password' => '',
+    'port' => '33306',
     'charset' => 'utf8'
 ];
